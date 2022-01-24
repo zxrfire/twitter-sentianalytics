6fb1e56c40601e6d5b24ae1c0d3cde333a5ac6ff
Bill Yang <billyang@umich.edu>
2022-01-23 23:08:51 -0500

    monke

--- Flask/twitter_posts_fetcher.py
+++ Flask/twitter_posts_fetcher.py
@@ -1,15 +1,17 @@
+import dotenv
 import json
 import requests
 import os
-import key
+dotenv.load_dotenv()
 
 twitter_url = 'https://api.twitter.com/2/tweets/search/recent?max_results=100&query='
-oath = {'Authorization': 'Bearer ' + key.bearer_Token}
+oath = {'Authorization': 'Bearer ' + os.environ.get('BEARER_TOKEN')}
 
 if __name__ == "__main__":
     twitter_url = twitter_url + 'nft'
     pag1 = requests.get(twitter_url, headers=oath)
     data = pag1.json()
-    with open('pag1.txt', 'w') as f:
-        f.write(json.dumps(data))
+    with open('pag1.json', 'w') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
 
